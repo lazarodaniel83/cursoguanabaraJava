63d14ccd29c6e1dfe241dfca8e6ec49bb230617d
lazaro souza <ldss158@gmail..com>
2020-01-08 23:16:45 -0300

    Exercicio vetores aula 14 java basico

---
+++ Vetor/Vetor03/src/vetor03/Vetor03.java
@@ -0,0 +1,21 @@
+
+package vetor03;
+
+import java.util.Arrays;
+
+
+public class Vetor03 {
+
+    
+    public static void main(String[] args)
+    {
+        double v[] = {3,5,2.75,9,-4.5};
+        Arrays.sort(v);
+        
+        for(double vl:v)
+        {
+            System.out.println(vl + "");
+        }
+    }
+    
+}
---
+++ Vetor/Vetor04/src/vetor04/Vetor04.java
@@ -0,0 +1,19 @@
+
+package vetor04;
+
+import java.util.Arrays;
+
+
+public class Vetor04 {
+
+    public static void main(String[] args)
+    {
+       int vet[] = {3,7,6,1,9,4,2};
+       for(int v:vet)
+            System.out.print(v+" ");
+       System.out.println("");
+       int p = Arrays.binarySearch(vet,1);
+        System.out.println("Encontrei o valor na posição "+p);
+    }
+    
+}
